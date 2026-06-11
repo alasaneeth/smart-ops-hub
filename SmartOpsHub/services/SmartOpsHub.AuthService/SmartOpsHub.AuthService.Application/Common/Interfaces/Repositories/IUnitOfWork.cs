2a2c37ed2d11d4180684cb2c7e9ed9a311280fa1
alasaneeth <alasaneeth22@gmail.com>
2026-06-11 14:20:16 +0530

    feat(application): add IUnitOfWork interface with Users and RefreshTokens repositories

---
+++ SmartOpsHub/services/SmartOpsHub.AuthService/SmartOpsHub.AuthService.Application/Common/Interfaces/Repositories/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using SmartOpsHub.AuthService.Application.Common.Interfaces.Repositories;
+
+namespace SmartOpsHub.AuthService.Application.Common.Interfaces;
+
+public interface IUnitOfWork : IDisposable
+{
+    IUserRepository Users { get; }
+    IRefreshTokenRepository RefreshTokens { get; }
+    Task<int> SaveChangesAsync();
+}
