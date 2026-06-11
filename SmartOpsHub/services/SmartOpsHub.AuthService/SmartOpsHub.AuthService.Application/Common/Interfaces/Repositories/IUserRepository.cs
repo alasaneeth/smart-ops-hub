3b00e04c128dd7a0e61c1e7be9e0a1d65eef20cb
alasaneeth <alasaneeth22@gmail.com>
2026-06-11 14:17:51 +0530

    feat(application): add IUserRepository interface

---
+++ SmartOpsHub/services/SmartOpsHub.AuthService/SmartOpsHub.AuthService.Application/Common/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using SmartOpsHub.AuthService.Domain.Entities;
+
+namespace SmartOpsHub.AuthService.Application.Common.Interfaces.Repositories;
+
+public interface IUserRepository
+{
+    Task<User?> GetByIdAsync(Guid id);
+    Task<User?> GetByEmailAsync(string email);
+    Task<IEnumerable<User>> GetAllAsync();
+    Task<User> AddAsync(User user);
+    Task UpdateAsync(User user);
+    Task DeleteAsync(Guid id);
+    Task<bool> ExistsAsync(string email);
+}
