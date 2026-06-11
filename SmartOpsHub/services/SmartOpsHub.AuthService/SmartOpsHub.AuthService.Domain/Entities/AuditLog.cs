6a55a9e5c2e4a9aa0d6db2c47cf7493f3a4346f4
alasaneeth <alasaneeth22@gmail.com>
2026-06-11 13:48:11 +0530

    feat(domain): add AuditLog entity for tracking user actions

---
+++ SmartOpsHub/services/SmartOpsHub.AuthService/SmartOpsHub.AuthService.Domain/Entities/AuditLog.cs
@@ -0,0 +1,15 @@
+﻿using SmartOpsHub.AuthService.Domain.Common;
+
+namespace SmartOpsHub.AuthService.Domain.Entities;
+
+public class AuditLog : BaseEntity
+{
+    public Guid UserId { get; set; }
+    public string Action { get; set; } = string.Empty;
+    public string? IpAddress { get; set; }
+    public string? UserAgent { get; set; }
+    public bool IsSuccess { get; set; } = true;
+
+    // Navigation Property
+    public User User { get; set; } = null!;
+}
