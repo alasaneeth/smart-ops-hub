4e12d389a1dbd8645606c5ea6dd25f9f55a85c60
alasaneeth <alasaneeth22@gmail.com>
2026-06-11 14:19:11 +0530

    feat(application): add IRefreshTokenRepository interface

---
+++ SmartOpsHub/services/SmartOpsHub.AuthService/SmartOpsHub.AuthService.Application/Common/Interfaces/Repositories/IRefreshTokenRepository.cs
@@ -0,0 +1,12 @@
+﻿using SmartOpsHub.AuthService.Domain.Entities;
+
+namespace SmartOpsHub.AuthService.Application.Common.Interfaces.Repositories;
+
+public interface IRefreshTokenRepository
+{
+    Task<RefreshToken?> GetByTokenAsync(string token);
+    Task<IEnumerable<RefreshToken>> GetByUserIdAsync(Guid userId);
+    Task<RefreshToken> AddAsync(RefreshToken refreshToken);
+    Task UpdateAsync(RefreshToken refreshToken);
+    Task RevokeAllUserTokensAsync(Guid userId, string reason);
+}
