d7199cb0429378f0dfdc5335ec5cf0a672452bda
alasaneeth <alasaneeth22@gmail.com>
2026-06-11 14:26:47 +0530

    feat(application): add IAuthService interface with Register, Login, RefreshToken and Logout

---
+++ SmartOpsHub/services/SmartOpsHub.AuthService/SmartOpsHub.AuthService.Application/Services/IAuthService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SmartOpsHub.AuthService.Application.DTOs.Auth;
+
+namespace SmartOpsHub.AuthService.Application.Common.Interfaces;
+
+public interface IAuthService
+{
+    Task<AuthResponseDto> RegisterAsync(RegisterRequestDto request);
+    Task<AuthResponseDto> LoginAsync(LoginRequestDto request);
+    Task<AuthResponseDto> RefreshTokenAsync(string refreshToken);
+    Task LogoutAsync(Guid userId);
+    Task<bool> RevokeTokenAsync(string refreshToken);
+}
